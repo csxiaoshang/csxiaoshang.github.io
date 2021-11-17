59261225117a670e1f59463a0cd4df65a52a7379
ashang <970090853@qq.com>
2021-11-17 12:46:24 +0800

    Update messagePush.sh

--- messagePush.sh
+++ messagePush.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 echo "time now is:"
-read hours
-if [ $hours -lt 12 ]
+#获取当前时间，格式是时分，例如当前是上午8：50，hh=850
+hh=`date '+%H%M'`
+if [ $hh -ge 730 -a $hh -le 1200 ]
 then
 echo "good morning"
 curl 'https://oapi.dingtalk.com/robot/send?access_token=3da81d20415a2ac6e3d59fa6c03441c53b3aee49a396f31c5a785e60e557cfd2' \
